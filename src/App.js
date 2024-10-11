f3d36cf3be018341e6bcf408e9706084ac43cfaa
Raymond Lu <raymondlu77@gmail.com>
2024-10-11 13:31:17 -0500

    adding all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import Appbar from "./components/Appbar";
+import Dog from "./components/Dog";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const current_theme = localStorage.getItem("current_theme");
+	const [theme, setTheme] = useState(current_theme ? current_theme : "light");
+
+	useEffect(() => {
+		localStorage.setItem("current_theme", theme);
+	}, [theme]);
+	return (
+		<div className={`container ${theme}`}>
+			<Appbar theme={theme} setTheme={setTheme} />
+			<Dog />
+		</div>
+	);
 }
 
 export default App;
