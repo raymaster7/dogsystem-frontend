50633956097e6979ffce4729f2b04203ffd2985f
Raymond Lu <54683014+raymaster7@users.noreply.github.com>
2024-10-21 15:42:06 -0500

    moved to right directory

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -6,6 +6,7 @@ import search_icon_light from "../assets/search-w.png";
 import search_icon_dark from "../assets/search-b.png";
 import toggle_light from "../assets/night.png";
 import toggle_dark from "../assets/day.png";
+import { Link } from "react-router-dom";
 const Appbar = ({ theme, setTheme }) => {
 	const toggle_mode = () => {
 		theme === "light" ? setTheme("dark") : setTheme("light");
@@ -19,10 +20,18 @@ const Appbar = ({ theme, setTheme }) => {
 				className="logo"
 			/>
 			<ul>
-				<li>Home</li>
-				<li>Dog Breeds</li>
-				<li>Dog Quiz</li>
-				<li>About</li>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/dogbreed">Dog Breed</Link>
+				</li>
+				<li>
+					<Link to="/dogquiz">Dog Quiz</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
 			</ul>
 			<div className="search-box">
 				<input type="text" placeholder="Search" />
---
+++ src/pages/Dog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dog = (props) => {
+	return (
+		<div className="child">
+			<img style={{ width: 300, height: 300 }} src={props.url} alt="" />
+		</div>
+	);
+};
+
+export default Dog;
